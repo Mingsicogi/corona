079001df8ae44694fcdb11fa7214951f6387c12f
Mingsicogi <codedoctor119@gmail.com>
2020-06-05 23:26:11 +0900

    + add account implements

--- src/main/java/com/mins/corona/api/account/AccountController.java
+++ src/main/java/com/mins/corona/api/account/AccountController.java
@@ -1,6 +1,7 @@
 package com.mins.corona.api.account;
 
 import com.mins.corona.api.account.dto.RedisDTO;
+import com.mins.corona.app.repository.AccountRepository;
 import com.mins.corona.common.service.RedisService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -11,9 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class Account {
+public class AccountController {
 
     private final RedisService redisService;
+    private final AccountRepository accountRepository;
 
     @PostMapping(value = "/redis/test", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> redisTest(@RequestBody RedisDTO param) {
