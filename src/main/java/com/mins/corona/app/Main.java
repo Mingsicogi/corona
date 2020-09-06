56aaee1505b213712d08247b3dc4613ec8c6f203
Mingsicogi <codedoctor119@gmail.com>
2020-09-06 13:28:03 +0900

    + change thymeleaf engine

--- src/main/java/com/mins/corona/app/Main.java
+++ src/main/java/com/mins/corona/app/Main.java
@@ -1,13 +1,26 @@
 package com.mins.corona.app;
 
+import com.mins.corona.app.service.NaverMapService;
+import com.mins.corona.utils.CommonUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
+@RequiredArgsConstructor
 public class Main {
 
-	@RequestMapping(value = {"/corona", "/corona/main"})
-	public String defaultPage() {
-		return "redirect:" + "/corona/whoareyou";
+	private final NaverMapService naverMapService;
+
+	@GetMapping(value = {"/", "/main", "", "/index"})
+	public String getPage(Model model) {
+		Optional<String> infecteeInfoListAsJsonString = CommonUtil.objectToString(naverMapService.getAllInfecteeList("infecteeInfoList"));
+		model.addAttribute("infecteeInfoJsonStr", infecteeInfoListAsJsonString.orElse("noData"));
+
+		return "main";
 	}
 }
